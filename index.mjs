86b7faa698c794778327bb6a64fd9708077ebf66
Claudio Yanes <me@claudio4.com>
2024-04-05 23:17:54 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,35 @@
+import fs from "fs/promises";
+import YAML from "yaml";
+
+
+const autogradingFile = await fs.readFile(".github/workflows/classroom.yml", { encoding: "utf-8" })
+const autograding = YAML.parse(autogradingFile);
+const steps = autograding?.jobs?.["run-autograding-tests"].steps;
+
+if (!steps) {
+  throw new Error("Could not find steps in the autograding file.");
+}
+
+const tests = steps
+  .filter(({ uses }) => (
+    uses.startsWith("education/autograding-io-grader") ||
+    uses.startsWith("education/autograding-command-grader")
+  ))
+  .map(({ with: t }) => ({
+    name: t["test-name"],
+    setup: t["setup-command"],
+    run: t.command,
+    input: t.input,
+    output: t["expected-output"],
+    comparison: t["comparison-method"],
+    timeout: t.timeout,
+    points: t["max-score"],
+  }))
+
+await fs.mkdir(".github/classroom", {
+  recursive: true
+});
+
+await fs.writeFile(".github/classroom/autograding.json", JSON.stringify(tests, null, 2));
+
+
